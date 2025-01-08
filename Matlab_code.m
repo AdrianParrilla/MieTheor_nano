8f298ab74d7d61c428dd6187ea84f5c9bc5af7f3
Adrian Parrilla <125877893+AdrianParrilla@users.noreply.github.com>
2025-01-08 11:50:32 +0100

    Update and rename Matlab_codig.m to Matlab_code.m

--- Matlab_code.m
+++ Matlab_code.m
@@ -1,22 +1,25 @@
 %% Variables
 
-siz = 50:1500;
+siz = 50:1500; % particle size
 r = siz./2;
-nm = 1.337;
-ns = 1.463;
-lambda= 488;
+nm = 1.337;  % refractive index of the media
+ns = 1.463;  % refractive index of the particle
+lambda= 488; % wavelenght of the illumination light
 k = (2*pi*nm)/lambda;
-NA = 1.018;
-nang = 200;
+NA = 1.018;  % numerical aperture of the detector
+nang = 200;  % number of angles to consider
 pang=linspace(0.706,2.430,nang);
 conv = 1;
-filename='df_Jan_apogee.xls';
+filename='df_apogee.xls';
 alpha = asind(NA./nm).*pi./180;
-% thetamin =  pi./2 - alpha  ; %angulos en radianes
+
+% thetamin =  pi./2 - alpha  ; %
 % thetamax =  pi./2 + alpha  ;  
 % phimin = @(theta) asin(sin(pi./2-alpha)./sin(theta)); 
 % phimax = @(phimin) pi-phimin ;
+
 %% Coated EV model
+% It considers a core-shell particle with different refractive indexes in the inner and outer parts.
 
 size = 50:1500;
 re = size./2 ; 
@@ -26,10 +29,12 @@ coatsph = [];
 RIi = 1.36 ;
 RIe = 1.48 ;
 nscoat = [RIi,RIe];
+
 %% Matrices
 
 ES = zeros(1,2,nang,length(r));
-%% Bucle
+
+%% Calculates the amplitude scattering matrix elements (S) of the Mie theory for each angle
 
 for rad = 1:length(r)
     [S, C, ang] = calcmie( r(rad), ns, nm, lambda, nang, 'ConvergenceFactor', 1);
@@ -39,7 +44,8 @@ for rad = 1:length(r)
     end
 
 end
-%% Bucle coat
+
+%% Same loop but for coated particles
 
 for rad = 1:length(size)
     coatsph = [ri(rad),re(rad)];
@@ -49,7 +55,8 @@ for rad = 1:length(size)
         ES(:,2,p,rad) = abs(S(1,1,p)).^2;
     end
 end
-%% Integration
+
+%% Integration step
 
 dif = (pang(end)-pang(1))./length(pang);
 scat = zeros(1,length(r));
@@ -76,7 +83,7 @@ for j = 1:length(r)
     disp(j)
 end
 
-%% Plot
+%% output plots
 
 scatlog = log10(scat);
 ds = [siz; scatlog]';
